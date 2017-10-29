0313d83e3fc7dce15afe2b5b1a581fa68048b548
Eric Anderson <AndersonAnderson@gmail.com>
2017-10-29 13:44:09 -0600

    cleanup

--- xchange-bitfinex/src/main/java/org/known/xchange/bitfinex/v2/service/BitfinexSignatureDigest.java
+++ xchange-bitfinex/src/main/java/org/known/xchange/bitfinex/v2/service/BitfinexSignatureDigest.java
@@ -1,13 +1,14 @@
 package org.known.xchange.bitfinex.v2.service;
 
-import net.iharder.Base64;
+import java.math.BigInteger;
+
+import javax.crypto.Mac;
+
 import org.knowm.xchange.service.BaseParamsDigest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import si.mazi.rescu.RestInvocation;
 
-import javax.crypto.Mac;
-import java.math.BigInteger;
+import si.mazi.rescu.RestInvocation;
 
 public class BitfinexSignatureDigest extends BaseParamsDigest {
 
@@ -30,12 +31,12 @@ public class BitfinexSignatureDigest extends BaseParamsDigest {
     return new BitfinexSignatureDigest(base64secretKey);
   }
 
-//  /**
-//   * Signature generation. See https://docs.bitfinex.com/v2/docs/rest-auth
-//   *
-//   * @param restInvocation
-//   * @return
-//   */
+  /**
+   * Signature generation. See https://docs.bitfinex.com/v2/docs/rest-auth
+   *
+   * @param restInvocation
+   * @return
+   */
   @Override
   public String digestParams(RestInvocation restInvocation) {
 
@@ -49,7 +50,7 @@ public class BitfinexSignatureDigest extends BaseParamsDigest {
     return digestParamsDirect(signature);
   }
 
-  public String digestParamsDirect(String value) {
+  protected String digestParamsDirect(String value) {
 
     Mac mac = getMac();
     mac.update(value.getBytes());
@@ -57,17 +58,4 @@ public class BitfinexSignatureDigest extends BaseParamsDigest {
     return String.format("%096x", new BigInteger(1, mac.doFinal()));
   }
 
-
-
-
-//  @Override
-//  public String digestParams(RestInvocation restInvocation) {
-//
-//    Mac mac = getMac();
-//    mac.update(restInvocation.getMethodPath().getBytes());
-//    mac.update(new byte[]{0});
-//    mac.update(restInvocation.getRequestBody().getBytes());
-//
-//    return Base64.encodeBytes(mac.doFinal()).trim();
-//  }
 }
